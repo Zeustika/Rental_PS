9f4cc5d62956dd624fda4498d23deb98da61b54a
Zeustika <149464441+Zeustika@users.noreply.github.com>
2024-11-27 00:12:14 +0700

    Add files via upload

---
+++ admin/mobil/tambah.php
@@ -0,0 +1,94 @@
+<?php
+/*
+  | Source Code Aplikasi Rental playstation PHP & MySQL
+  | 
+  | @package   : rental_playstation
+  | @file	   : tambah.php 
+  | @author    : fauzan1892 / Fauzan Falah
+  | @copyright : Copyright (c) 2017-2021 Codekop.com (https://www.codekop.com)
+  | @blog      : https://www.codekop.com/products/source-code-aplikasi-rental-playstation-php-mysql-7.html 
+  | 
+  | 
+  | 
+  | 
+ */
+    require '../../koneksi/koneksi.php';
+    $title_web = 'Tambah playstation';
+    include '../header.php';
+    if(empty($_SESSION['USER']))
+    {
+        session_start();
+    }
+?>
+
+
+<br>
+<div class="container">
+    <div class="card">
+        <div class="card-header text-white bg-primary">
+            <h4 class="card-title">
+                Tambah playstation
+                <div class="float-right">
+                    <a class="btn btn-warning" href="mobil.php" role="button">Kembali</a>
+                </div>
+            </h4>
+        </div>
+        <div class="card-body">
+            <div class="container">
+                <form method="post" action="proses.php?aksi=tambah" enctype="multipart/form-data">
+                    <div class="row">
+
+                        <div class="col-sm-6">
+
+                            <div class="form-group row">
+                                <label class="col-sm-3">Type penyewaan</label>
+                                <input type="text" class="form-control col-sm-9" name="type_sewa" placeholder="Isi Type penyewaan">
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3">jenis_ps playstation</label>
+                                <input type="text" class="form-control col-sm-9" name="jenis_ps" placeholder="Isi jenis_ps">
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3">Harga</label>
+                                <input type="text" class="form-control col-sm-9" name="harga" placeholder="Isi Harga">
+                            </div>
+
+                        </div>
+
+                        <div class="col-sm-6">
+
+                            <div class="form-group row">
+                                <label class="col-sm-3">Deskripsi</label>
+                                <input type="text" class="form-control col-sm-9" name="deskripsi" placeholder="Isi Deskripsi">
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3">Status</label>
+                                <select class="form-control col-sm-9" name="status">
+                                    <option value="" disabled selected>Pilih Status</option>
+                                    <option>Tersedia</option>
+                                    <option>Tidak Tersedia</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-3">Gambar</label>
+                                <input type="file" accept="image/*" class="form-control col-sm-9" name="gambar" placeholder="Isi Gambar">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="float-right">
+                        <button class="btn btn-primary" role="button" type="submit">
+                            Simpan
+                        </button>
+                    </div>
+                </form>       
+            </div>
+        </div>
+    </div>
+</div>
+<?php  include '../footer.php';?>
---
+++ rental_ps.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Nov 2024 pada 20.15
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rental_ps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `booking`
+--
+
+CREATE TABLE `booking` (
+  `id_booking` int(11) NOT NULL,
+  `kode_booking` varchar(255) NOT NULL,
+  `id_login` int(11) NOT NULL,
+  `id_ps` int(11) NOT NULL,
+  `ktp` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_tlp` varchar(15) NOT NULL,
+  `tanggal` varchar(255) NOT NULL,
+  `lama_sewa` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  `konfirmasi_pembayaran` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `booking`
+--
+
+INSERT INTO `booking` (`id_booking`, `kode_booking`, `id_login`, `id_ps`, `ktp`, `nama`, `alamat`, `no_tlp`, `tanggal`, `lama_sewa`, `total_harga`, `konfirmasi_pembayaran`, `tgl_input`) VALUES
+(1, '1576329294', 3, 5, '231423123', 'Krisna', 'Bekasi', '08132312321', '2019-12-28', 2, 400000, 'Pembayaran di terima', '2019-12-14'),
+(2, '1576671989', 3, 5, '231423', 'Krisna Waskita', 'Bekasi Ujung Harapan', '082391273127', '2019-12-20', 2, 400525, 'Pembayaran di terima', '2019-12-18'),
+(3, '1642998828', 3, 5, '1283821832813', 'Krisna', 'Bekasi', '089618173609', '2022-01-26', 4, 800743, 'Pembayaran di terima', '2022-01-24');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `infoweb`
+--
+
+CREATE TABLE `infoweb` (
+  `id` int(11) NOT NULL,
+  `nama_rental` varchar(255) DEFAULT NULL,
+  `telp` varchar(15) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `no_rek` text DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `infoweb`
+--
+
+INSERT INTO `infoweb` (`id`, `nama_rental`, `telp`, `alamat`, `email`, `no_rek`, `updated_at`) VALUES
+(1, 'Rental Playstation', '0812934345643', 'cilendek Kab. Tasikmalaya', 'Ikhsanmiftah@gmail.com', 'BRI Ikhsan Miftah Fauzan 123123213123', '2022-01-24 04:57:29');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `nama_pengguna` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`id_login`, `nama_pengguna`, `username`, `password`, `level`) VALUES
+(1, 'Anang', 'admin', 'fe01ce2a7fbac8fafaed7c982a04e229', 'admin'),
+(3, 'Krisna Waskita', 'demo', '12345', 'pengguna'),
+(4, 'admin1', 'aa', '827ccb0eea8a706c4c34a16891f84e7b', 'admin'),
+(5, 'aa', 'user', '827ccb0eea8a706c4c34a16891f84e7b', 'pengguna');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_booking` int(255) NOT NULL,
+  `no_rekening` int(255) NOT NULL,
+  `nama_rekening` varchar(255) NOT NULL,
+  `nominal` int(255) NOT NULL,
+  `tanggal` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_booking`, `no_rekening`, `nama_rekening`, `nominal`, `tanggal`) VALUES
+(3, 1, 2131241, 'Krisna Aldi Waskito', 400000, '2019-12-14'),
+(4, 2, 2131241, 'Krisna Aldi Waskito', 400525, '2019-12-18'),
+(5, 3, 13213, 'Fauzan Falah', 800743, '2022-01-24');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id_pengembalian` int(11) NOT NULL,
+  `kode_booking` varchar(255) NOT NULL,
+  `tanggal` varchar(255) NOT NULL,
+  `denda` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `playstation`
+--
+
+CREATE TABLE `playstation` (
+  `id_ps` int(11) NOT NULL,
+  `type_sewa` varchar(255) NOT NULL,
+  `jenis_ps` varchar(255) NOT NULL,
+  `harga` int(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `playstation`
+--
+
+INSERT INTO `playstation` (`id_ps`, `type_sewa`, `jenis_ps`, `harga`, `deskripsi`, `status`, `gambar`) VALUES
+(6, 'bawa kerumah', 'ps4', 500000, 'Baru', 'Tersedia', 'ps4.jpg'),
+(7, 'ditempat', 'ps5', 200000, 'aaaa', 'Tersedia', '1732559088.jpg'),
+(8, 'gftfhd', 'dafse', 434545, 'sdfsdfb', 'Tersedia', '1732559671.jpg'),
+(9, 'gftfhd', 'dafse', 434545, 'sdfsdfb', 'Tersedia', '1732559793.jpg'),
+(10, 'sdg', 'dsgdg', 0, 'gsd', 'Tersedia', '1732562052.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id_booking`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`);
+
+--
+-- Indeks untuk tabel `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`id_pengembalian`);
+
+--
+-- Indeks untuk tabel `playstation`
+--
+ALTER TABLE `playstation`
+  ADD PRIMARY KEY (`id_ps`) USING BTREE;
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id_booking` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `playstation`
+--
+ALTER TABLE `playstation`
+  MODIFY `id_ps` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
